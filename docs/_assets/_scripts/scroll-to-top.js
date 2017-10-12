d11db5a08fed88f727959cde029ebd1a27e02d4a
job_talk <job-talk@users.noreply.github.com>
2017-10-12 12:18:39 -0500

    Adjusted services text size, added border-radius to scoll up, sel style

--- docs/_assets/_scripts/scroll-to-top.js
+++ docs/_assets/_scripts/scroll-to-top.js
@@ -1,17 +1,5 @@
-// document.querySelector('#scroll-top').addEventListener('click', function() {
-//   var scrollToTop = window.setInterval(function() {
-//     var pos = window.pageYOffset;
-//
-//     if (pos > 0) {
-//       window.scrollTo(0, pos - 20); // how far to scroll on each step
-//     } else {
-//       window.clearInterval(scrollToTop);
-//     }
-//   }, 16); // how fast to scroll (this equals roughly 60 fps)
-// })
-
 var scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 700,
+  speed: 800,
   offset: 0,
   easing: 'easeInOutCubic'
 });
