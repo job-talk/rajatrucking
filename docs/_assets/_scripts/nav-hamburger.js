42eeb846612136ba73c9541fa406b98408834b02
job_talk <job-talk@users.noreply.github.com>
2017-11-13 10:17:20 -0600

    added esc, click link to close nav

--- docs/_assets/_scripts/nav-hamburger.js
+++ docs/_assets/_scripts/nav-hamburger.js
@@ -1,7 +1,7 @@
 // Get elements
 var navbarBurger = document.querySelectorAll('.navbar-burger');
 var navbarMenu   = document.querySelectorAll('.navbar-menu');
-
+var navMenuItems = document.querySelectorAll('.navbar-menu .navbar-item');
 
 
 // Declare functions
@@ -25,64 +25,7 @@ function toggleMenu() {
   }
 }
 
-function isMenuOpen() {
-  if (navbarMenu.classList.contains('is-active') && navbarBurger.classList.contains('is-active')) {
-    console.log('Menu is active/open');
-    return true;
-  }
-  else {
-    console.log('Menu is closed');
-    return false;
-  }
-}
-
-
-
-// Close menu when open using ESC key, toggle X
-document.onkeydown = function(evt) {
-  evt = evt || window.event;
-
-  // if ($navbarBurgers.length > 0) {
-  //
-  //   $navbarBurgers.forEach(function($el) {
-  //     if (evt.keyCode == 27) {
-  //
-  //       var target = $el.dataset.target;
-  //       var $target = document.getElementById(target);
-  //
-  //       if ($el.classList.contains('is-active') && $target.classList.contains('is-active')) {
-  //         $el.classList.toggle('is-active');
-  //         $target.classList.toggle('is-active');
-  //       }
-  //
-  //     }
-  //   });
-  //
-  // }
-
-  // if ((evt.keyCode == 27) && (isMenuOpen() == true)) {
-  //   toggleMenu();
-  // }
-};
-
-
-//Close menu with click outside of menu
-for (i = 0; i < navbarMenu.length; i++) {
-  navbarMenu[i].addEventListener('click', function(event) {
-    if (event.target.closest('.navbar')) {
-      // navbarMenu.classList.remove('is-active');
-      // do nothing
-      console.log('click');
-    } else {
-      closeMenuClick();
-    }
-
-  });
-}
-
-// Close menu when clicking a list item
-var navMenuItems = document.querySelectorAll('.navbar-menu .navbar-item');
-for (i = 0; i < navMenuItems.length; i++) {
+function closeMenuOnLinkClick() {
   navMenuItems[i].addEventListener('click', function() {
 
     if (this.parentNode.classList.contains('is-active')) {
@@ -95,9 +38,54 @@ for (i = 0; i < navMenuItems.length; i++) {
   });
 }
 
+//Close menu with click outside of menu
+function closeMenuonOutsideClick() {
+  for (i = 0; i < navbarMenu.length; i++) {
+    navbarMenu[i].addEventListener('touchstart', function(event) {
+      if (event.target.closest('.navbar')) {
+        // navbarMenu.classList.remove('is-active');
+        // navbarBurger.classList.remove('is-active');
+        // do nothing
+        console.log('click outside');
+      } else {
+        navbarMenu.classList.remove('is-active');
+        navbarBurger.classList.remove('is-active');
+        // closeMenuClick();
+      }
+
+    });
+  }
+}
+
+
+// Close menu when clicking a list item
+for (i = 0; i < navMenuItems.length; i++) {
+  closeMenuOnLinkClick();
+}
+
+
 
 // Event listeners
 // Add navbar hamburger functionality and 'X' animation
 document.addEventListener('DOMContentLoaded', function() {
   toggleMenu();
 });
+
+// Close menu when open using ESC key, toggle X
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+
+  if (navbarBurger.length > 0) {
+    navbarBurger.forEach(function($el) {
+      if (evt.keyCode == 27) {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        if ($el.classList.contains('is-active') && $target.classList.contains('is-active')) {
+          $el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+        }
+      }
+    });
+  }
+};
